bba947d20168d5875f27619f16e77015746f3e03
julio <juliofacundo2018@hotmail.com>
2026-01-27 23:45:25 -0300

    guardar cambios en database

--- database/schem.sql
+++
@@ -1,9 +0,0 @@
- CREATE TABLE viajes (
-    id serial, 
-    destino varchar (50) not null, 
-    presupuesto int not null 
-    );
-
- CREATE TABLE equipamiento (
-    id SERIAL, 
-   nombre VARCHAR(50));
