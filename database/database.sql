fd50288c7b239b9c161e75739fe988cda8bea871
julio <juliofacundo2018@hotmail.com>
2026-01-29 22:47:50 -0300

    archivos recuperados

---
+++ database/database.sql
@@ -0,0 +1,13 @@
+const { Pool } = require('pg')
+const pool = new Pool({
+host: 'localhost',
+user: 'postgres',
+password: 'nicanoR2018',
+database: 'plan_de_viajes',
+allowExitOnIdle: true
+})
+const getDate = async () => {
+const result = await pool.query("SELECT NOW()")
+console.log(result)
+}
+getDate()
